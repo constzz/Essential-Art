9b55d1bfe3e098b306e3dcc0ae7161c85da851a6
Konstantin Bezzemelnyi <konstantin.bezzemelnyi@gmail.com>
2022-11-05 08:56:50 +0200

    Initial Commit

---
+++ Essential Art/Essential_Art.h
@@ -0,0 +1,18 @@
+//
+//  Essential_Art.h
+//  Essential Art
+//
+//  Created by Konstantin Bezzemelnyi on 05.11.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Essential_Art.
+FOUNDATION_EXPORT double Essential_ArtVersionNumber;
+
+//! Project version string for Essential_Art.
+FOUNDATION_EXPORT const unsigned char Essential_ArtVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Essential_Art/PublicHeader.h>
+
+
