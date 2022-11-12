9206bfea39b05b09e47026478bd37aa6a82f37ee
Konstantin Bezzemelnyi <konstantin.bezzemelnyi@gmail.com>
2022-11-12 20:21:40 +0200

    Remove incorrect tests

--- Essential Art/Artwork Cache/Infrastructure/Core Data/ManagedArtwork.swift
+++ Essential Art/Artwork Cache/Infrastructure/Core Data/ManagedArtwork.swift
@@ -14,20 +14,3 @@ class ManagedArtwork: NSManagedObject {
     @NSManaged var imageURL: URL
     @NSManaged var title: String
 }
-
-extension ManagedArtwork {
-    static func find(in context: NSManagedObjectContext) throws -> ManagedArtworksCache? {
-        let request = NSFetchRequest<ManagedArtworksCache>(entityName: entity().name!)
-        request.returnsObjectsAsFaults = false
-        return try context.fetch(request).first
-    }
-    
-    static func deleteCache(in context: NSManagedObjectContext) throws {
-        try find(in: context).map(context.delete).map(context.save)
-    }
-    
-    static func newUniqueInstance(in context: NSManagedObjectContext) throws -> ManagedArtworksCache {
-        try deleteCache(in: context)
-        return ManagedArtworksCache(context: context)
-    }
-}
