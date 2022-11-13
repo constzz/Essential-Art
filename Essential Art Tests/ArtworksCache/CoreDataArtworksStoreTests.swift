ed1e819624ce285e62ba121d7a68d35b17a44a41
Konstantin Bezzemelnyi <konstantin.bezzemelnyi@gmail.com>
2022-11-13 12:51:29 +0200

    Test that CoreDataArtworksStore insert delivers no error on empty cache

--- Essential Art Tests/ArtworksCache/CoreDataArtworksStoreTests.swift
+++ Essential Art Tests/ArtworksCache/CoreDataArtworksStoreTests.swift
@@ -40,6 +40,15 @@ class CoreDataArtworksStoreTests: XCTestCase {
         expect(sut, toRetrieveTwice: .success(.some((artworks, timestamp))))
     }
     
+    func test_insert_deliversNoErrorOnEmptyCache() throws {
+        let sut = makeSUT()
+        let artworks = uniqueArtworks().models
+        let timestamp = Date()
+        try sut.insert(artworks, timestamp: timestamp)
+        
+        expect(sut, toRetrieve: .success((artworks, timestamp)))
+    }
+    
     private func makeSUT() -> ArtworksStore {
         let storeURL = URL(fileURLWithPath: "/dev/null") // path to temporary directory
         let sut = try! CoreDataArtworksStore(storeURL: storeURL)
