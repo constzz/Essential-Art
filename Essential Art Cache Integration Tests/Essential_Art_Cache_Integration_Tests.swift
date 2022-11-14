ce94112635ff9074d761e1ef19745a550a1fb63f
Konstantin Bezzemelnyi <konstantin.bezzemelnyi@gmail.com>
2022-11-14 17:39:55 +0200

    Add Essential Art Cache Integration Tests test target

---
+++ Essential Art Cache Integration Tests/Essential_Art_Cache_Integration_Tests.swift
@@ -0,0 +1,12 @@
+//
+//  Essential_Art_Cache_Integration_Tests.swift
+//  Essential Art Cache Integration Tests
+//
+//  Created by Konstantin Bezzemelnyi on 14.11.2022.
+//
+
+import XCTest
+import Essential_Art
+
+class Essential_Art_Cache_Integration_Tests: XCTestCase {
+}
