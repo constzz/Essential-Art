5bbbe3897416a16a33bbf715dc39cd8860ddc95f
Konstantin Bezzemelnyi <konstantin.bezzemelnyi@gmail.com>
2022-11-15 17:59:04 +0200

    Add ResourceLoadingView and ResourceErrorView with their models. Test presenter on start loading send nil error message to error view and sends isLoading true to loading view

--- Essential Art Tests/Shared  Presentation/LoadResourcePresenterTests.swift
+++ Essential Art Tests/Shared  Presentation/LoadResourcePresenterTests.swift
@@ -8,8 +8,37 @@
 import Foundation
 import XCTest
 
+struct ResourceLoadingViewModel {
+    let isLoading: Bool
+}
+
+protocol ResourceLoadingView {
+    func display(_ viewModel: ResourceLoadingViewModel)
+}
+
+struct ResourceErrorViewModel {
+    let errorMessage: String?
+    static let noError = ResourceErrorViewModel(errorMessage: nil)
+}
+
+protocol ResourceErrorView {
+    func display(_ viewModel: ResourceErrorViewModel)
+}
+
 final class LoadResourcePresenter {
     
+    private let loadingView: ResourceLoadingView
+    private let errorView: ResourceErrorView
+    
+    init(loadingView: ResourceLoadingView, errorView: ResourceErrorView) {
+        self.loadingView = loadingView
+        self.errorView = errorView
+    }
+    
+    func didStartLoading() {
+        errorView.display(.noError)
+        loadingView.display(.init(isLoading: true))
+    }
 }
 
 class LoadResourcePresenterTests: XCTestCase {
@@ -19,15 +48,36 @@ class LoadResourcePresenterTests: XCTestCase {
         XCTAssertTrue(view.messages.isEmpty, "Expected no view messages")
     }
     
+    func test_didStartLoading_displaysNoErrorMessageAndStartsLoading() {
+        let (sut, view) = makeSUT()
+        
+        sut.didStartLoading()
+        
+        XCTAssertEqual(view.messages, [
+            .display(errorMessage: .none),
+            .display(isLoading: true)
+        ])
+    }
+    
     private func makeSUT() -> (LoadResourcePresenter, ViewSpy) {
-        let presenter = LoadResourcePresenter()
         let view = ViewSpy()
+        let presenter = LoadResourcePresenter(loadingView: view, errorView: view)
         return (presenter, view)
     }
     
-    class ViewSpy {
-        enum Message {
+    class ViewSpy: ResourceLoadingView, ResourceErrorView {
+        enum Message: Equatable {
+            case display(isLoading: Bool)
+            case display(errorMessage: String?)
         }
         var messages = [Message]()
+        
+        func display(_ viewModel: ResourceLoadingViewModel) {
+            messages.append(.display(isLoading: viewModel.isLoading))
+        }
+        
+        func display(_ viewModel: ResourceErrorViewModel) {
+            messages.append(.display(errorMessage: viewModel.errorMessage))
+        }
     }
 }
