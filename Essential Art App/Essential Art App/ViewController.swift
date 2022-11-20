88a1a9228655f71af6f74980347764edd760130c
Konstantin <77053754+constzz@users.noreply.github.com>
2022-11-20 18:24:13 +0200

    Separate main app target and app components target (#5)

---
+++ Essential Art App/Essential Art App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Essential Art App
+//
+//  Created by Konstantin Bezzemelnyi on 20.11.2022.
+//
+
+import UIKit
+import Essential_Art_iOS
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
