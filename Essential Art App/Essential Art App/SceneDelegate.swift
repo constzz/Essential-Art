99e7902b5511d927f0698beb8606d47533ec830f
Konstantin Bezzemelnyi <konstantin.bezzemelnyi@gmail.com>
2022-11-20 19:30:46 +0200

    Configure programmatic layout instead of storyboards as main screen

--- Essential Art App/Essential Art App/SceneDelegate.swift
+++ Essential Art App/Essential Art App/SceneDelegate.swift
@@ -10,9 +10,18 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
+    
+    private lazy var navigationController = UINavigationController()
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let scene = (scene as? UIWindowScene) else { return }
 
+        window = UIWindow(windowScene: scene)
+        configureWindow()
     }
+    
+    func configureWindow() {
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
     }
 }
