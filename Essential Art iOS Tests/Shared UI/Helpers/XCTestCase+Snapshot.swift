fff4c45f45ad51148baef2bccb57169c9349d144
Konstantin Bezzemelnyi <konstantin.bezzemelnyi@gmail.com>
2022-11-20 13:04:37 +0200

    Update iOS Ci with script to see which snapshots failing on CI simulator

--- Essential Art iOS Tests/Shared UI/Helpers/XCTestCase+Snapshot.swift
+++ Essential Art iOS Tests/Shared UI/Helpers/XCTestCase+Snapshot.swift
@@ -26,7 +26,20 @@ extension XCTestCase {
             
             try? snapshotData.write(to: temporarySnapshotURL)
             
-            XCTFail("New snapshot does not match stored snapshot. New snapshot URL: \(temporarySnapshotURL), Stored snapshot URL: \(snapshotURL)", file: file, line: line)
+            let errorMessage = "New snapshot does not match stored snapshot. New snapshot URL: \(temporarySnapshotURL), Stored snapshot URL: \(snapshotURL)"
+            
+            var issue = XCTIssue(type: .assertionFailure, compactDescription: errorMessage)
+            let receivedAttachment = XCTAttachment(contentsOfFile: temporarySnapshotURL)
+            receivedAttachment.name = "RECEIVED RESULT"
+            
+            let expectedAttachment = XCTAttachment(contentsOfFile: snapshotURL)
+            expectedAttachment.name = "EXPECTED RESULT"
+            
+            issue.add(receivedAttachment)
+            issue.add(expectedAttachment)
+            self.record(issue)
+            
+            XCTFail(errorMessage, file: file, line: line)
         }
     }
     
