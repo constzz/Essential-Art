2ec166f6ac9bd79a8066c438374514944784d173
Konstantin Bezzemelnyi <konstantin.bezzemelnyi@gmail.com>
2022-11-21 10:08:05 +0200

    Add combine extensions for Paginated

--- Essential Art App/Essential Art App/CombineHelpers.swift
+++ Essential Art App/Essential Art App/CombineHelpers.swift
@@ -10,6 +10,32 @@ import Combine
 import Essential_Art
 import Essential_Art_iOS
 
+public extension Paginated {
+    init(items: [Item], loadMorePublisher: (() -> AnyPublisher<Self, Error>)?) {
+        self.init(items: items, loadMore: loadMorePublisher.map { publisher in
+            return { completion in
+                publisher().subscribe(Subscribers.Sink(receiveCompletion: { result in
+                    if case let .failure(error) = result {
+                        completion(.failure(error))
+                    }
+                }, receiveValue: { result in
+                    completion(.success(result))
+                }))
+            }
+        })
+    }
+    
+    var loadMorePublisher: (() -> AnyPublisher<Self, Error>)? {
+        guard let loadMore = loadMore else { return nil }
+        
+        return {
+            Deferred {
+                Future(loadMore)
+            }.eraseToAnyPublisher()
+        }
+    }
+}
+
 extension Publisher {
     func dispatchOnMainQueue() -> AnyPublisher<Output, Failure> {
         receive(on: DispatchQueue.immediateWhenOnMainQueueScheduler).eraseToAnyPublisher()
