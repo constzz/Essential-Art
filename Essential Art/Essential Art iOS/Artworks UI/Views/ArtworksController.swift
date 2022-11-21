dd92baded171ab45585b3055fab118afe730310e
Konstantin <77053754+constzz@users.noreply.github.com>
2022-11-21 19:57:39 +0200

    Update ArtworksController.swift

--- Essential Art/Essential Art iOS/Artworks UI/Views/ArtworksController.swift
+++ Essential Art/Essential Art iOS/Artworks UI/Views/ArtworksController.swift
@@ -11,6 +11,7 @@ public class ArtworksController: ListViewController {
     
     public init() {
         super.init(nibName: nil, bundle: nil)
+    }
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
