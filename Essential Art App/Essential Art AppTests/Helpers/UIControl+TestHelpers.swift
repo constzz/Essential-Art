ddec5dada9f10091a7b426a608feb7d8c6ce0e81
Konstantin Bezzemelnyi <konstantin.bezzemelnyi@gmail.com>
2022-11-21 10:15:36 +0200

    Create UIControl+TestHelpers.swift

---
+++ Essential Art App/Essential Art AppTests/Helpers/UIControl+TestHelpers.swift
@@ -0,0 +1,18 @@
+//
+//  UIControl+TestHelpers.swift
+//  Essential Art AppTests
+//
+//  Created by Konstantin Bezzemelnyi on 20.11.2022.
+//
+
+import UIKit
+
+extension UIControl {
+    func simulate(event: UIControl.Event) {
+        allTargets.forEach { target in
+            actions(forTarget: target, forControlEvent: event)?.forEach {
+                (target as NSObject).perform(Selector($0))
+            }
+        }
+    }
+}
