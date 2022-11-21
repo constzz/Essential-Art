f52a2c3bd973ded5c22379e085ab71c9ddddb205
Konstantin Bezzemelnyi <konstantin.bezzemelnyi@gmail.com>
2022-11-21 19:35:42 +0200

    Add memory leak helpers to Essential Art AppTests

---
+++ Essential Art App/Essential Art AppTests/Helpers/XCTestCase+TestHelpers.swift
@@ -0,0 +1,17 @@
+//
+//  XCTestCase+TestHelpers.swift
+//  Essential Art AppTests
+//
+//  Created by Konstantin Bezzemelnyi on 21.11.2022.
+//
+
+import XCTest
+
+extension XCTestCase {
+    func trackForMemoryLeaks(_ instance: AnyObject, file: StaticString = #filePath, line: UInt = #line) {
+        addTeardownBlock { [weak instance] in
+            XCTAssertNil(instance, "Instance should have been deallocated. Potential memory leak.", file: file, line: line)
+        }
+    }
+}
+
