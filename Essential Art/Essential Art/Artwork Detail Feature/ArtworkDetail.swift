53251231c328419cccc79e7e530796ed80c67fac
Konstantin Bezzemelnyi <konstantin.bezzemelnyi@gmail.com>
2022-11-22 12:28:35 +0200

    Create Artwork Detail Model

---
+++ Essential Art/Essential Art/Artwork Detail Feature/ArtworkDetail.swift
@@ -0,0 +1,15 @@
+//
+//  ArtworkDetail.swift
+//  Essential Art
+//
+//  Created by Konstantin Bezzemelnyi on 22.11.2022.
+//
+
+import Foundation
+
+public struct ArtworkDetail {
+    public let title: String
+    public let artist: String
+    public let description: String
+    public let imageURL: URL
+}
