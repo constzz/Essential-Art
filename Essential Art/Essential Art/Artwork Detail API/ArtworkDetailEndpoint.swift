3083f77edf246252cf8535dc87f5e03849f7dbdc
Konstantin Bezzemelnyi <konstantin.bezzemelnyi@gmail.com>
2022-11-22 12:41:06 +0200

    Create ArtworkDetailEndpoint and test get image detail url

---
+++ Essential Art/Essential Art/Artwork Detail API/ArtworkDetailEndpoint.swift
@@ -0,0 +1,19 @@
+//
+//  ArtworkDetailEndpoint.swift
+//  Essential Art
+//
+//  Created by Konstantin Bezzemelnyi on 22.11.2022.
+//
+
+import Foundation
+
+public enum ArtworkDetailEndpoint {
+    case get(id: String)
+    
+    public func url(baseURL: URL) -> URL {
+        switch self {
+        case let .get(id):
+            return baseURL.appendingPathComponent("api/v1/artworks/\(id)")
+        }
+    }
+}
